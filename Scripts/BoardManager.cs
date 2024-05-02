2f9dbde6b48610f55996c6bc0c2efddb4ee44ccb
RedSaber <d4quillen@icloud.com>
2024-05-02 11:06:05 -0400

    Added README to explain project. Added license information to all scripts.

--- Scripts/BoardManager.cs
+++ Scripts/BoardManager.cs
@@ -1,4 +1,12 @@
-// Created by Daniel in 1985
+/*
+ * Copyright 2024, Daniel from Saber C++
+ * All rights reserved.
+ * 
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. 
+ *
+ * (The BSD license basically means you can do whatever you want with this code (modify it, sell it, etc.) but you can't complain to me if it doesn't work perfectly and you can't use my name in association with anything you do with this code.)
+ */
 
 using Godot;
 using System;
